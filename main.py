ec881e47cc61b0d03fbdf2047adccbdae6b8549f
yanlc <xush2014@hotmail.com>
2020-05-22 22:27:28 +0800

    增加了下载进度窗口

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class NetMusicGUI(object):
         label = Label(root, text='输入歌手页面地址：', font=('微软雅黑', 23))
         label.grid(row=0, column=0)
         self.url = ''
+        self.downloading = 0
         self.music_dict = {}
         self.music_list = {}
         self.entry = Entry(root, font=('微软雅黑', 23), width=25)
@@ -34,6 +35,7 @@ class NetMusicGUI(object):
         button1.grid(row=2, column=0, sticky=W)
         button2 = Button(root, text='开始下载', font=('微软雅黑', 20), command=self.downloadMusic)
         button2.grid(row=2, column=1, sticky=W)
+
         root.mainloop()
 
     def downloadMusic(self):
@@ -45,7 +47,13 @@ class NetMusicGUI(object):
         singer = r.find('title').text.strip(' - 歌手 - 网易云音乐')
         songDownload_Url = 'http://music.163.com/song/media/outer/url?id=' + songID
         path = 'music\\' + self.music_dict[songID] + ' - ' + singer + '.mp3'
-        urlretrieve(songDownload_Url, path)
+        urlretrieve(songDownload_Url, path, self.loading)
+        downPercent = Tk()
+        downPercent.title('下载进度---' + self.music_dict[songID]+' - '+singer+'.mp3')
+        song_Str = self.music_dict[songID]+' - '+singer+'.mp3'
+        downPercent.geometry('1212x520+700+400')
+        downLabel = Label(downPercent, text=song_Str+':'+'已经下载:{}'.format(self.downloading), font=('微软雅黑', 23))
+        downLabel.grid(row=0, column=0)
 
     def getList(self):
         self.text.delete(0, END)
@@ -67,6 +75,12 @@ class NetMusicGUI(object):
             self.text.insert(END, music_name)
             self.text.see(END)
             self.text.update()
+
+    def loading(self, blocknum, blocksize, totalsize):
+        percent = 100.0 * blocknum * blocksize / totalsize
+        if percent > 100:
+            percent = 100
+        self.downloading = '%.2f%%' % percent
 
 
 if __name__ == '__main__':
